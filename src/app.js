b0970a1d6648838bc5d25307db64b3c7aacc6207
MakandarSahil <sahilmakandar15@gmail.com>
2025-03-02 21:03:48 +0530

    .

--- src/app.js
+++ src/app.js
@@ -2,29 +2,40 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 const app = express();
 
-// cors policy 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}))
+// cors policy
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 // json data limit
-app.use(express.json({
-  limit: "16kb" 
-}));
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
 
-app.use(express.urlencoded({
-  extended: true,
-  limit: "16kb" 
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
 app.use(express.static("public"));
 
+// crud on cookies of user
+app.use(cookieParser());
 
-// crud on cookies of user 
-app.use(cookieParser())
+//routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 
 export default app;
